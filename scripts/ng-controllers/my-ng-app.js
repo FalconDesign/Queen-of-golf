6d60c9edf11c3977818b08a3a37ec48b1e32d682
FalconDesign <hebgax0@gmail.com>
2016-04-29 20:20:24 +0300

    Created secons slide

---
+++ scripts/ng-controllers/my-ng-app.js
@@ -0,0 +1,64 @@
+var app = angular.module('page_app', ['ngSanitize']);
+
+app.controller('PageController', ['$scope',function ($scope) {
+
+
+
+
+$scope.articles = [
+
+  art = [
+    {number: 8,
+      number_title: 'Eight',
+      header_title: 'Wangari does all the things eight year old’s do',
+      content:' She goes to class during the week and has homework which she endeavors to finish by Friday afternoon so she can play golf.',
+      quote: '“It is a lot of running, we have to make it to practice a bunch of times in a week. There are a lot of early mornings and cramped schedules, especially ahead of a big tournament, but Chanelle loves golf, which makes it all worth it,” ',
+      smn_sass: 'says Maureen Wangari, Chanelle’s mother.',
+      girl_says: 'Wangari says she enjoys school very much.',
+      girl_quote: '“I love math, numbers are really fun. They help me with golf, you can see how you’re doing in the game, just by using math problems.”'}
+  ],
+  art = [
+    {number: 7,
+      number_title: 'Seven',
+      header_title: 'Long attention span – but for golf',
+      content:' When Wangari is on the golf course, the game is the only thing on her mind. Yes! No getting distracted. When she plays at the US Kids Golf World Championship, it will be her first time to travel to the US.',
+    quote: null,
+    smn_sass: 'This is what Wangari said when I asked her what she wanted to know about North Carolina. <span>“How is the humidity? It will affect the distance I can hit the ball. I really can’t wait to go to Disney World, right after the competition we are going there and I am so excited!”</span> ',
+    girl_says: null,
+    girl_quote: null }
+  ],
+  art = [
+    {number: 6,
+      number_title: 'Six',
+      header_title: 'Travels because of golf',
+      content: 'To qualify for the US Kids Golf World Championship, Wangari had to be top three in the Africa Junior Golf Championship that was held in South Africa in September 2014. She has played in Malaysia and will travel to Italy in October of 2015 to play.',
+      smn_sass: '<span> “I’m very excited to go to Europe to golf. I haven’t been there before and I want to play on their courses, I hear they’re really beautiful,” she says. </span>'}
+  ],
+  art = [
+    {number: 5,
+      number_title:'Five',
+      header_title: '',
+      content: 'It was love at first sight for Wangari. None of her family members play golf. She just loves the game.'}
+  ],
+  art = [
+    {number: 4,
+      number_title:'Four',
+      header_title: '',
+      content: 'Practice session are four to five times a week at Vet Lab or Muthaiga Golf Club.'}
+  ],
+  art = [
+    {number: 3,
+      number_title:'Three',
+      header_title: '',
+      content: 'Wangari’s coach is Charles Farrar, a former pro with the British PGA who has been a golf instructor for nearly two decades. During the session, Farrar makes changes to Wangari’s swings and shot. After a poor shot Farrar makes her adjust her hands a few degrees, barely noticeable to anyone watching.'}
+  ],
+  art = [
+    {number: 2,
+      number_title:'Two',
+      header_title: '',
+      content: 'She not only wants to be the best but the very best golfer in the world. She is eyeing the top slot during the US Kids Golf World Championship.'}
+  ]
+];
+
+
+}])
